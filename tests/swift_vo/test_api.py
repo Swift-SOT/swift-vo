7ffdcc541de11f73bfd5f9ba9c93ee0dac03a509
Jamie Kennea <jak574@gmail.com>
2025-03-29 08:19:41 +0100

    fix: unit tests, precommit

--- tests/swift_vo/test_api.py
+++ tests/swift_vo/test_api.py
@@ -1,13 +1,16 @@
-from email.policy import HTTP
+import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
-from swift_vo.objobssap.api import app, parse_pos, parse_time, parse_min_obs
-import pytest
+
+from swift_vo.objobssap.api import app, parse_min_obs, parse_pos, parse_time
 
 client = TestClient(app)
 
 
 def test_parse_pos():
+    """
+    Test the parse_pos function.
+    """
     pos = parse_pos("10.5,20.3")
     assert pos.s_ra == 10.5
     assert pos.s_dec == 20.3
@@ -17,6 +20,9 @@ def test_parse_pos():
 
 
 def test_parse_time():
+    """
+    Test the parse_time function.
+    """
     time = parse_time("60000/60001")
     assert time.t_min == 60000.0
     assert time.t_max == 60001.0
@@ -26,11 +32,17 @@ def test_parse_time():
 
 
 def test_parse_min_obs():
+    """
+    Test the parse_min_obs function.
+    """
     assert parse_min_obs(1500) == 1500.0
     assert parse_min_obs(0) == 0.0
 
 
 def test_objvissap_endpoint():
+    """
+    Test the ObjObsSAP endpoint with valid parameters.
+    """
     response = client.get(
         "/objobssap/query",
         params={"POS": "10.5,20.3", "TIME": "60000/60001", "MIN_OBS": "1500", "MAX_REC": "100"},
@@ -41,6 +53,9 @@ def test_objvissap_endpoint():
 
 
 def test_objvissap_invalid_params():
+    """
+    Test invalid parameters for the ObjObsSAP endpoint.
+    """
     response = client.get(
         "/objobssap/query", params={"POS": "invalid", "TIME": "60000/60001", "MIN_OBS": "1.5"}
     )
